bb11c7dff6d7aa177f337b9c86156af4b903c6a6
Orion Adrian <orion.adrian@gmail.com>
2021-02-05 01:14:32 -0500

    Remove cssbaline from app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { CssBaseline } from '@material-ui/core';
 import { useEffect } from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
