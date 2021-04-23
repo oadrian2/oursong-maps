a025a1a3752544fed4eb03541af2de7224177692
Orion Adrian <orion.adrian@gmail.com>
2021-04-23 01:07:08 -0400

    Click on image deselects tokens

--- src/map/MapImage.js
+++ src/map/MapImage.js
@@ -1,8 +1,21 @@
-import { useSelector } from 'react-redux';
+import { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectMapImage } from './mapSlice';
+import { tokenDeselected } from './selectionSlice';
 
 export function MapImage() {
+  const dispatch = useDispatch();
+
+  const onClick = useCallback(
+    (event) => {
+      if (event.button === 2) return;
+
+      dispatch(tokenDeselected(null));
+    },
+    [dispatch]
+  );
+
   const { src, width } = useSelector(selectMapImage);
 
-  return <img draggable="false" style={{ width }} src={src} alt="the map" />;
+  return <img draggable="false" style={{ width }} src={src} alt="the map" onClick={onClick} />;
 }
