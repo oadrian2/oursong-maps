d38f1cf05e360460a631920993e39a3ff00303f8
Orion Adrian <orion.adrian@gmail.com>
2022-02-17 20:14:08 -0500

    Add connection tag (GM/Player)

--- src/session/ConnectionInfo.tsx
+++ src/session/ConnectionInfo.tsx
@@ -1,14 +1,15 @@
 import { Box } from '@mui/material';
 import { useRecoilValue } from 'recoil';
-import { userIdState } from "../app/userState";
+import { isGMState, userIdState } from "../app/userState";
 
 
 export function ConnectionInfo() {
   const userId = useRecoilValue(userIdState);
+  const isGM = useRecoilValue(isGMState);
 
   return (
     <Box position="fixed" bottom={16} left={16} bgcolor="black" color="white" p={1}>
-      {userId}
+      {userId} {isGM ? ' (GM)' : '(Player)'}
     </Box>
   );
 }
