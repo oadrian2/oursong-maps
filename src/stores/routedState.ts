0f08d88af05b5911a5954bf92b8d00aada902241
Orion Adrian <orion.adrian@gmail.com>
2022-11-30 17:03:55 -0500

    Bump minor versions

--- src/stores/routedState.ts
+++ src/stores/routedState.ts
@@ -1,15 +1,15 @@
-import { nullable, string } from '@recoiljs/refine';
+import { string } from '@recoiljs/refine';
 import { atom, errorSelector } from 'recoil';
 import { syncEffect } from 'recoil-sync';
 
 export const routedGameKeyState = atom<string>({
   key: 'RoutedGame',
   default: errorSelector(`Value for 'game' missing in route.`),
-  effects: [syncEffect({ refine: nullable(string()), itemKey: 'game', storeKey: 'context' })],
+  effects: [syncEffect({ refine: string(), itemKey: 'game', storeKey: 'context' })],
 });
 
 export const routeMapKeyState = atom<string>({
   key: 'RoutedMap',
   default: errorSelector(`Value for 'map' missing in route.`),
-  effects: [syncEffect({ refine: nullable(string()), itemKey: 'map', storeKey: 'context' })],
+  effects: [syncEffect({ refine: string(), itemKey: 'map', storeKey: 'context' })],
 });
